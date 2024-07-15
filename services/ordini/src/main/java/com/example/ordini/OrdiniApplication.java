1f71dda6919929fd3833bf405a9c0b3be00cb02a
m_adriani <m.adriani@jdk.it>
2024-07-15 15:38:06 +0200

    inizio ordine service

---
+++ services/ordini/src/main/java/com/example/ordini/OrdiniApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdiniApplication.class, args);
+	}
+
+}
---
+++ services/ordini/src/test/java/com/example/ordini/OrdiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
